72064ef5e6a4c6a33d2ec8846df805ee0dbb580c
unknown <idecjj@hendrix.edu>
2022-04-21 13:46:32 -0400

    small changes to getTimeseries

--- tests/testthat/test_roleExperiment.R
+++ tests/testthat/test_roleExperiment.R
@@ -4,6 +4,11 @@ context('several model runs work')
 
 # works
 test_that('experiment of 1 run and 100 iters works', {
-  experiment <- dummyModel(R=TRUE,run=TRUE,fill_ts=TRUE,niters=100,return_experiment=TRUE)
-  experiment@modelRuns[[1]]@timeseries[[10]]
+  experiment <- roleR::dummyModel(R=TRUE,run=TRUE,fill_ts=TRUE,niters=100,return_experiment=TRUE)
+  getTimeseries(experiment@modelRuns[[1]],valueName="abundanceSp")
+  #writeRoleExperiment(experiment,fileName="test")
+  #test <- readRDS("test.roleexperiment")
+  #experiment@modelRuns[[1]]@timeseries[[10]]
 })
+
+#ecolottery contains list of dataframes
