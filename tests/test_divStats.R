acadcca52020db6e5a2ac071dd337d953b16d4c5
ajrominger <ajrominger@gmail.com>
2022-07-28 14:02:07 -0400

    added a test for sum stats as applied to experiments

--- tests/test_divStats.R
+++ tests/test_divStats.R
@@ -1,20 +1,20 @@
 
 library(testthat)
 library(roleR)
-context('get final state works')
+context('div stat methods work')
 
 # WORKS AS WRITTEN
-test_that('get final state runs without error'){
+test_that('div stat funs run without error on one roleData object'){
     params <- roleParams(individuals_local = 100, individuals_meta = 1000,
                          species_meta = 500, speciation_local = 0.1, #0.1
                          speciation_meta = 1, extinction_meta = 0.8, #1
                          trait_sigma=1,comp_sigma = 0.1, env_sigma=0.1,dispersal_prob = 0.1, mutation_rate = 0.01, # comp sigma =1 causes probs to be bad
                          equilib_escape = 1, num_basepairs = 250,
-                         init_type = 'oceanic_island', niter = 1000, niterTimestep = 1000)
+                         init_type = 'oceanic_island', niter = 1000, niterTimestep = 10)
     model <- roleModel(params)
     model <- iterModel(model,F)
     
-    datFrmMod <- slot(model, 'modelSteps')[[10]]
+    datFrmMod <- slot(model, 'modelSteps')[[100]]
     
     
     hillAbund(datFrmMod)
@@ -27,3 +27,23 @@ test_that('get final state runs without error'){
     
     richness(datFrmMod)
 }
+
+
+test_that('div stat funs run without error through getSumStat function'){
+    params <- roleParams(individuals_local = 100, individuals_meta = 1000,
+                         species_meta = 500, speciation_local = 0.1, #0.1
+                         speciation_meta = 1, extinction_meta = 0.8, #1
+                         trait_sigma=1,comp_sigma = 0.1, env_sigma=0.1,dispersal_prob = 0.1, mutation_rate = 0.01, # comp sigma =1 causes probs to be bad
+                         equilib_escape = 1, num_basepairs = 250,
+                         init_type = 'oceanic_island', niter = 1000, niterTimestep = 10)
+    model <- roleModel(params)
+    model <- iterModel(model,F)
+    
+    expr <- as(model, 'roleExperiment')
+    
+    ss <- getSumStats(expr, funs = list(abund = rawAbundance, rich = richness, 
+                                        hillAbund = hillAbund, 
+                                        hillTrait = hillTrait))
+}
+
+
