1127e685e074a977059f1de0d4d52361d3cf021f
ajrominger <ajrominger@gmail.com>
2022-08-18 13:44:54 -0400

    added params set up

--- inst/ajr_sandbox.R
+++ inst/ajr_sandbox.R
@@ -1,3 +1,13 @@
+library(roleR)
+
+p <- roleParams(individuals_local = 100, individuals_meta = 10000, 
+                species_meta = 5, speciation_local = 0, speciation_meta = 1, 
+                extinction_meta = 0.5, trait_sigma = 1, env_sigma = 1, 
+                comp_sigma = 1, dispersal_prob = 0.1, mutation_rate = 0.01, 
+                equilib_escape = 1, alpha = 1000, num_basepairs = 500, 
+                init_type = 'oceanic_island', 
+                niter = 1000, niterTimestep = 200)
+
 m <- roleModel(p)
 
 m@modelSteps[[1]]@localComm@indSpecies
