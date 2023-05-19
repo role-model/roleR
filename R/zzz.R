e09ffba2903b03791b39dfd521976486ad4c4c5d
ajrominger <ajrominger@gmail.com>
2023-05-19 15:08:16 -0400

    trying out options, nothing working so far

--- R/zzz.R
+++ R/zzz.R
@@ -5,3 +5,25 @@
 #               packages="msprime",
 #               channel="conda-forge"),error=function(e) {})
 # #install_miniconda(force=TRUE,update=FALSE)
+
+# .onLoad <- function(libname, pkgname) {
+#     packageStartupMessage('linking to python dependencies...')
+#     
+#     browser()
+#     
+#     reticulate::use_condaenv("r-reticulate")
+#     pypack <- reticulate::py_list_packages()
+#     pyreqs <- c('msprime', 'newick')
+#     
+#     booboo <- pyreqs[!(pyreqs %in% pypack$package)]
+#     
+#     if(length(booboo) > 0) {
+#         s <- paste('missing python dependencies:', 
+#                    paste(booboo, collapse = ', '))
+#         packageStartupMessage(s)
+#     } else {
+#         packageStartupMessage('linking successful')
+#     }
+#     
+# }
+# 
