d0bdf3b806a79b332fe5a2d696dc7be412b8961a
Isaac Overcast <isaac.overcast@gmail.com>
2023-05-20 10:02:31 -0400

    Create role_rocker_intall.sh

---
+++ inst/role_rocker_intall.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+apt update; apt install libgsl-dev libpng-dev -y
+
+sudo -u rstudio bash -i -c '\
+    cd ~
+    pwd
+    if uname -a | grep aarch64 >> /dev/null;
+    then
+        echo "Detected arm"
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-aarch64.sh
+        bash Miniconda3-latest-Linux-aarch64.sh -b
+    elif uname -a | grep x86_64 >> /dev/null;
+    then
+        echo "Detected x86_64"
+        wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+        bash Miniconda3-latest-Linux-x86_64.sh -b
+    else
+        echo "Failed to detect host architecture. Must be arm (Mac) or x86_64 (Linux/Windows)"
+    fi
+    ./miniconda3/bin/conda init
+'
+
+sudo -u rstudio bash -i -c '\
+    cd ~
+    which python
+    conda install -c bioconda python-newick -y
+    conda install -c conda-forge pandas -y
+    rm -rf msprime
+    git clone --recurse-submodules https://github.com/tskit-dev/msprime.git
+    cd msprime; pip install .
+'
+
+echo "install.packages(\"remotes\")
+install.packages(\"reticulate\")
+library(reticulate)
+library(remotes)
+reticulate::use_condaenv('base') # <- Tell reticulate to use the externally installed conda env
+# NB: use reticulate::conda_list() to check the env name if this is not correct, but it should be by default
+remotes::install_github('role-model/roleR', force=TRUE)" > tmp.txt
+
+sudo -u rstudio bash -i -c 'Rscript tmp.txt'
+
+echo "library(reticulate)
+reticulate::use_condaenv('base')" > /home/rstudio/.Rprofile
+chown rstudio:rstudio /home/rstudio/.Rprofile
