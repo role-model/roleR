bc34875e7bfdf5c65a43677008d8dc3165faa1e3
ajrominger <ajrominger@gmail.com>
2023-11-10 14:09:06 -1000

    fleshing out test

--- tests/testthat/test_rcpp_comm.R
+++ tests/testthat/test_rcpp_comm.R
@@ -1,7 +1,15 @@
 # test that you can give comm to rcpp and what it gives you back is the same
 
-p <- roleParams()
+p <- roleParams(niter = 10000000, 
+                niterTimestep = 1000000,
+                species_meta = 500, 
+                individuals_meta = 100000, 
+                individuals_local = 5000)
 
 m <- roleModel(p)
 
-roleR:::roleCommTester(m@modelSteps[[1]], p)
+# roleR:::roleCommTester(m@modelSteps[[1]], p)
+
+mr <- runRole(m)
+
+# getSumStats(mr, funs = list("hill_abund" = hillAbund, "rich" = richness))
