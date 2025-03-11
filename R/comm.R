b32d319e4cd527ee34c39793666369c4f1679cb4
ajrominger <ajrominger@gmail.com>
2025-03-11 09:30:01 -1000

    allowing for matrix of traits

--- R/comm.R
+++ R/comm.R
@@ -24,7 +24,7 @@
 
 setClass('localComm',
          slots = c(indSpecies = 'numeric',
-                   indTrait = 'numeric', # convert to matrix for multiple trts
+                   indTrait = 'matrix', # convert to matrix for multiple trts
                    indSeqs = 'character',
                    spGenDiv = 'numeric',
                    # spAbundHarmMean = 'numeric',
@@ -78,7 +78,7 @@ localComm <- function(indSpecies, indTrait, indSeqs) {
 
 setClass('metaComm',
          slots = c(spAbund = 'numeric',
-                   spTrait = 'numeric'))
+                   spTrait = 'matrix'))
 
 
 # constructor 
