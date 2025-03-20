031cf55cf4459c748dd15285c867c0e3e883e69d
ajrominger <ajrominger@gmail.com>
2025-03-20 08:22:54 -1000

    adding a test of comp mat calculation for when all traits are equal

--- tests/testthat/test_rcpp_comp.R
+++ tests/testthat/test_rcpp_comp.R
@@ -18,3 +18,26 @@ test_that("rcpp competition matrix calculation works", {
                            "up_low_equal" = d[upper.tri(d)], 
                            "dist_correct" = dh))
 })
+
+
+test_that("rcpp competition matrix calculation works when all traits equal", {
+    # trait data, rows are individuals
+    trt <- matrix(-3, nrow = 3, ncol = 1)
+    
+    # competitive distance matrix
+    s <- 1.5
+    d <- roleR:::compMatCalcTest(x = trt, sigC = s)
+    
+    # distances calculated by hand
+    dh <- dist(trt) |> as.matrix()
+    dh <- exp(-(dh / s)^2)[2:3, 1]
+    names(dh) <- NULL
+    
+    expect_equivalent(list("diag_equal" = diag(d), 
+                           "up_low_equal" = d[lower.tri(d)], 
+                           "dist_correct" = d[2:3, 1]), 
+                      list("diag_equal" = rep(0, 3), 
+                           "up_low_equal" = d[upper.tri(d)], 
+                           "dist_correct" = dh))
+})
+
