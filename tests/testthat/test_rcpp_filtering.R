a2b84104c2442fcd4812781a605010b62955c10d
ajrominger <ajrominger@gmail.com>
2025-04-01 13:13:26 -1000

    adding tests for partial neutrality and trait evolution

--- tests/testthat/test_rcpp_filtering.R
+++ tests/testthat/test_rcpp_filtering.R
@@ -106,7 +106,7 @@ test_that("less dispersal slows approach to monodominance under filtering", {
 
 test_that("more neutral slows approach to monodominance under filtering", {
     pMoreNeut <- pFilter
-    pMoreNeut@neut_delta <- 0.9
+    pMoreNeut@neut_delta <- 0.5
     
     m <- roleModel(pMoreNeut) |> customizeMod()
     r <- runRole(m)
@@ -120,30 +120,59 @@ test_that("more neutral slows approach to monodominance under filtering", {
                       min(which(s2neut > 0.95)), 
                       100 + length(s2neut))
     
-    
-    # expect_gt(fptNeut, fpt)
-    expect_true(TRUE)
+    expect_gt(fptNeut, fpt)
 })
 
 
-test_that("wider `env_sigma` slows approach to monodom under filtering", {
-    m@params@env_sigma <- 10
+test_that("when there is trait evolution, populations move toward envOpt", {
+    pTrtEvo <- roleParams(niter = 100000,
+                          niterTimestep = 1000, 
+                          species_meta = 3, 
+                          individuals_meta = 100, 
+                          individuals_local = 100, 
+                          speciation_local = 0, # no new species
+                          dispersal_prob = 0.01, # immigration erases trt evo
+                          trait_sigma = 50, # big
+                          env_optim = 0, 
+                          env_sigma = 0.1,
+                          neut_delta = 0.1, 
+                          env_comp_delta = 1)
     
-    r <- runRole(m)
+    mTrtEvo <- roleModel(pTrtEvo) |> customizeMod()
+    rTrtEvo <- runRole(mTrtEvo)
     
-    s2WideS <- sapply(r@modelSteps, function(x) {
-        mean(x@localComm@indSpecies == 2)
+    trtEvo <- sapply(rTrtEvo@modelSteps, function(x) {
+        spp <- factor(x@localComm@indSpecies, 1:x@phylo@n)
+        tt <- tapply(x@localComm@indTrait, spp, mean)
+        tt
     })
+
+    # gather info so we can compare ancestral trait and new trait to optim
+    ancTrt <- mTrtEvo@modelSteps[[1]]@metaComm@spTrait[1, 1]
+    newTrt <- mean(trtEvo[1, ], na.rm = TRUE)
+    e <- 0 # hard coding the env optim, might cause breaking
     
-    plot(s2WideS)
-    lines(s2)
-    
-    # first passage time to near saturation of sp 2
-    fptWideS <- ifelse(any(s2WideS > 0.95), 
-                       min(which(s2WideS > 0.95)), 
-                       100 + length(s2WideS))
-    
-    
-    # expect_gt(fptWideS, fpt)
-    expect_true(TRUE)
+    expect_gt(abs(ancTrt - e), abs(newTrt - e))
 })
+
+## behavior of wider sig is not what I thought, so leave this one alone for now
+# test_that("wider `env_sigma` slows approach to monodom under filtering", {
+#     pWider <- pFilter
+#     pWider@env_sigma <- 10
+# 
+#     m <- roleModel(pWider) |> customizeMod()
+#     r <- runRole(m)
+#     
+#     s2WideS <- sapply(r@modelSteps, function(x) {
+#         mean(x@localComm@indSpecies == 2)
+#     })
+#     
+#     # first passage time to near saturation of sp 2
+#     fptWideS <- ifelse(any(s2WideS > 0.95), 
+#                        min(which(s2WideS > 0.95)), 
+#                        100 + length(s2WideS))
+#     
+#     
+#     expect_gt(fptWideS, fpt)
+# })
+
