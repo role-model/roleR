7b2fd564aa6218f68148482e110dafd208292ccc
ajrominger <ajrominger@gmail.com>
2025-06-04 12:10:25 -1000

    figured out standard way of handling tip names

--- inst/python/role_msprime.py
+++ inst/python/role_msprime.py
@@ -107,191 +107,192 @@ def sim_role(Jm,
         time_units = "myr",
         generation_time = 1
     )
-    
+
     # update msprime demography with local populations budding off from their
-    # metacommunity parental populations and create a list of sample sets that 
+    # metacommunity parental populations and create a list of sample sets that
     # determine when observations are made
-    
+
     sampset = [] # empty list to hold sample sets
-    
+
     for sp in meta_p.keys():
         # we track the ancestral population with `this_anc` which will update
         # after each addition of a local population
-        this_anc = f"t{sp}" # hard coding `t` will not work with specaition !!!!!!!!!!!!!!!!!!!!
-        
+        # we hard code `t` because we pass a phylo with all "t" prefixes
+        this_anc = f"t{sp}"
+
         # object to track current local and meta pops (only initializing here)
         this_loc = f"l_0_{sp}"
         this_meta = f"m_0_{sp}"
-        
+
         for t in range(len(local_sad_ts)):
             # `t` is the *index* for snapshots
-            
+
             # extract abundance for this sp in this t
             this_abund = local_sad_ts[t][sp]
-            
+
             # only bother with adding to demography if non-0 abundance
-            if this_abund > 0: 
-                
+            if this_abund > 0:
+
                 # tags to note _m_eta/_l_ocal and _t_ime
-                meta_sp = f"m_{t}_{sp}" 
+                meta_sp = f"m_{t}_{sp}"
                 local_sp = f"l_{t}_{sp}"
-                    
-                # set value of `add_yn` (determine if adding populations) based 
+
+                # set value of `add_yn` (determine if adding populations) based
                 # on whether t == 0 (always add in initial time step)
                 add_yn = True if t == 0 else False
-                
+
                 # if t!= 0, determine if we still add a new pop for this lineage
                 if (not add_yn) and local_tdiv_ts[t][sp] != local_tdiv_ts[t - 1][sp]:
                     add_yn = True
-                
+
                 if add_yn:
-                    # track the pops with `this_loc` and `this_meta` which will 
-                    # update at each addition of a local comm population; we   
-                    # have to track separately because we might need to take a 
+                    # track the pops with `this_loc` and `this_meta` which will
+                    # update at each addition of a local comm population; we
+                    # have to track separately because we might need to take a
                     # sample of this same pop across multiple snapshots
                     this_loc = local_sp
                     this_meta = meta_sp
-                    
+
                     # needed info for adding pops
                     local_Ne = local_hm_ts[t][sp] * alpha # harmonic mean = Ne
                     split_time = curtime - local_tdiv_ts[t][sp] # backward time
-                    
+
                     demography.add_population(
-                        name = this_meta, 
+                        name = this_meta,
                         initial_size = meta_Nes[sp]
                     )
-                    
+
                     demography.add_population(
-                        name = this_loc, 
+                        name = this_loc,
                         initial_size = local_Ne
                     )
-                    
-                    # when adding a pop need to make sure the ancestral pop is  
-                    # the previous metacomm pop, this may not be the same as 
-                    # `sp` so we use `this_anc` which initializes as `sp` but 
+
+                    # when adding a pop need to make sure the ancestral pop is
+                    # the previous metacomm pop, this may not be the same as
+                    # `sp` so we use `this_anc` which initializes as `sp` but
                     # then updates
                     demography.add_population_split(
-                        time = split_time, 
-                        derived = [this_meta, this_loc], 
+                        time = split_time,
+                        derived = [this_meta, this_loc],
                         ancestral = this_anc # note we use `this_anc`
                     )
-                    
-                    # strong colonization bottleneck cause only one individual  
+
+                    # strong colonization bottleneck cause only one individual
                     # at time of colonization
                     demography.add_simple_bottleneck(
-                        time = split_time, 
-                        population = this_loc, 
+                        time = split_time,
+                        population = this_loc,
                         proportion = 1
                     )
-                    
-                    # add migration 
+
+                    # add migration
                     # source is local, dest is meta (cause backward time)
                     # rate = \frac{m p^{(i)}_{meta}}{2 \alpha \eta^{(i)}_{local}}
                     demography.set_migration_rate(
-                        source = this_loc, 
-                        dest = this_meta, 
+                        source = this_loc,
+                        dest = this_meta,
                         rate = m * meta_p[sp] / (2 * alpha * local_p_ts[t][sp])
                     )
-                     
+
                     # only now update `this_anc`
                     this_anc = this_meta
-                
-                else: 
-                    # if weʻre not adding a lineage, but abund is non-zero, we 
+
+                else:
+                    # if weʻre not adding a lineage, but abund is non-zero, we
                     # still need to update population size and migration rate
-                    
+
                     demography.add_population_parameters_change(
                         time = curtime - gens[t],
-                        population = this_loc, 
+                        population = this_loc,
                         initial_size = local_hm_ts[t][sp] * alpha
                     )
-                    
+
                     demography.add_migration_rate_change(
                         time = curtime - gens[t],
-                        source = this_loc, 
-                        dest = this_meta, 
+                        source = this_loc,
+                        dest = this_meta,
                         rate = m * meta_p[sp] / (2 * alpha * local_p_ts[t][sp])
                     )
-                
+
                 # create sample set for this sp in this time step whether or not
-                # we are adding a new lineage 
+                # we are adding a new lineage
                 # if we add a new lineage, `this_loc` will reflect current timestep
-                # if we donʻt add new, `this_loc` will reflect last timestep 
+                # if we donʻt add new, `this_loc` will reflect last timestep
                 # where we did add a lineage
-                
+
                 if gens[t] == 0:
                     samp_time = curtime - 1
                 else:
                     samp_time = curtime - gens[t] # because backward time
-                
+
                 this_samp = msprime.SampleSet(
-                    this_abund, 
-                    population = this_loc, 
+                    this_abund,
+                    population = this_loc,
                     time = samp_time
                 )
-                    
+
                 sampset.append(this_samp)
-    
-    
-    # make sure demographic events are in proper ascending order 
+
+
+    # make sure demographic events are in proper ascending order
     # (backward in time)
     demography.sort_events()
 
 
     # simulate ancestry with specific sampling times
     ts = msprime.sim_ancestry(
-        samples = sampset, 
-        demography = demography, 
-        ploidy = 1, 
-        sequence_length = sequence_length 
+        samples = sampset,
+        demography = demography,
+        ploidy = 1,
+        sequence_length = sequence_length
     )
-    
+
     # simulate mutations
-    mts = msprime.sim_mutations(ts, rate = mu) 
-    
+    mts = msprime.sim_mutations(ts, rate = mu)
+
     # node table with all info
     ntab = mts.tables.nodes
-    
+
     # dataframe of needed info for sample nodes only
     df_ind = pd.DataFrame({
-        "ids": ntab.individual, 
-        "pops": ntab.population, 
+        "ids": ntab.individual,
+        "pops": ntab.population,
         "time": ntab.time})
     df_ind = df_ind[df_ind["ids"] > -1] # NOTE!!! this only works if `ploidy = 1`
-    
+
     # we used a hack for time at the root (`curtime - gen[t] - 1`), now we need
     # to add back that 1
     df_ind.loc[df_ind["time"] == curtime - 1, "time"] = curtime
-    
+
     # now flip time from backward direction to forward
     df_ind["time"] = curtime - df_ind["time"]
-    
-    # get actual names of the populations 
+
+    # get actual names of the populations
     pop_names = pd.Series([pop.metadata["name"] for pop in mts.tables.populations])
     df_ind["name"] = df_ind["pops"].map(pop_names)
-    
+
     # get just species names from "name" column
     df_ind["sp_id"] = df_ind["name"].str.split('_').str[-1]
-    
+
     # get simulated sequences (this hangs a suprising while)
     df_ind["seq_alignment"] = list(mts.alignments(
-        samples = df_ind["ids"].tolist(), 
+        samples = df_ind["ids"].tolist(),
         reference_sequence = tskit.random_nucleotides(ts.sequence_length)
     ))
-    
+
     # aggregate df so rows are species and column of ids is a column of lists
     df_spp = df_ind.groupby(["time", "sp_id"])['ids'].apply(list).reset_index()
-    
+
     # get diversities and Tajima for each pop in each time
     df_spp["pi"] = mts.diversity(df_spp["ids"].tolist())
     df_spp["tajimas_D"] = mts.Tajimas_D(df_spp["ids"].tolist())
 
     # all we need from `df_ind` is time, sp_id, and seqs
     df_ind = df_ind[["time", "sp_id", "seq_alignment"]]
-    
+
     # all we need from `df_ind` is time, sp_id, pi, and taj
     df_spp = df_spp[["time", "sp_id", "pi", "tajimas_D"]]
-    
-    
+
+
     return df_ind, df_spp
 
