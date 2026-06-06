57bce640e3557c9293d25b83a2102273bb95da00
ajrominger <ajrominger@gmail.com>
2026-06-05 14:38:28 -1000

    turned bare code into tests

--- tests/testthat/test_process.R
+++ tests/testthat/test_process.R
@@ -1,218 +1,290 @@
 # test competition modulated by traits ----
 
 test_that("intraspecific trait variation increases abundance of that species", {
-    
-})
+    # sp1: all indis at the same trait = heavy within-species competition
+    # sp2: inds spread across trait space = low within-species competition
 
+    n <- 5000
+    J <- 100
+    p <- roleParams(
+        niter = n,
+        niterTimestep = n,
+        species_meta = 2,
+        individuals_meta = 100,
+        individuals_local = J,
+        dispersal_prob = 0, # closed community: no immigration 
+        speciation_local = 0,
+        trait_sigma = 1e-8, # traits barely evolve, initial variation preserved
+        comp_sigma = 0.5,
+        env_sigma = 1,
+        neut_delta = 0,
+        env_comp_delta = 0 # pure competition
+    )
+
+    m <- roleModel(p)
+
+    m@modelSteps[[1]]@metaComm@spAbund <- c(0.5, 0.5)
+    m@modelSteps[[1]]@metaComm@spTrait <- matrix(c(0, 0), ncol = 1)
+
+    # equal starting abundances: 50 each
+    m@modelSteps[[1]]@localComm@indSpecies <- c(rep(1L, J / 2), rep(2L, J / 2))
+
+    # sp1: zero variation (all at trait = 0) → each individual competes with 49 others
+    # sp2: high variation (spread from -5 to +5) → each individual has few nearby competitors
+    trt_sp1 <- rep(0, J / 2)
+    trt_sp2 <- seq(-5, 5, length.out = J / 2)
+    m@modelSteps[[1]]@localComm@indTrait <- matrix(c(trt_sp1, trt_sp2), ncol = 1)
+
+    mr <- runRole(m)
+
+    n_sp2_final <- sum(mr@modelSteps[[2]]@localComm@indSpecies == 2)
+
+    # sp2 should end up more abundant than its initial 50%
+    expect_gt(n_sp2_final, J / 2)
+})
 
 
 # test filtering (closest to optimum wins) ----
 
-n <- 10000
-indsLocs <- 100
-p <- roleParams(niter = n, 
-                niterTimestep = n / 2,
-                species_meta = 3, 
-                individuals_meta = 100, 
-                individuals_local = indsLocs, 
-                dispersal_prob = 1,
-                speciation_local = 0,
-                comp_sigma = 0.5,
-                env_sigma = 0.5,
-                neut_delta = 0, 
-                env_comp_delta = 1)
+test_that("filtering: species closest to env optim takes over", {
+    n <- 10000
+    indsLocs <- 100
+    p <- roleParams(niter = n,
+                    niterTimestep = n / 2,
+                    species_meta = 3,
+                    individuals_meta = 100,
+                    individuals_local = indsLocs,
+                    dispersal_prob = 1,
+                    speciation_local = 0,
+                    comp_sigma = 0.5,
+                    env_sigma = 0.5,
+                    neut_delta = 0,
+                    env_comp_delta = 1)
 
-m <- roleModel(p)
+    m <- roleModel(p)
 
-# update the model to specific config for testing:
-# in metacomm, spp 1 and 2 have equal abundance, sp 3 has 0 abund
-m@modelSteps[[1]]@metaComm@spAbund <- c(0.5, 0.5, 0)
+    # spp 1 and 2 have equal abundance, sp 3 has 0 abund
+    m@modelSteps[[1]]@metaComm@spAbund <- c(0.5, 0.5, 0)
 
-# in metacomm, spp 1 matches optim perfectly, spp 2 and 3 are far from optim
-m@modelSteps[[1]]@metaComm@spTrait <- matrix(c(0, 10, 10), ncol = 1)
+    # sp 1 matches optim perfectly, spp 2 and 3 are far from optim
+    m@modelSteps[[1]]@metaComm@spTrait <- matrix(c(0, 10, 10), ncol = 1)
 
-# init of local comm is all sp 3
-m@modelSteps[[1]]@localComm@indSpecies <- rep(3, indsLocs)
-m@modelSteps[[1]]@localComm@indTrait <- matrix(10, nrow = indsLocs, ncol = 1)
+    # init: all sp 3
+    m@modelSteps[[1]]@localComm@indSpecies <- rep(3, indsLocs)
+    m@modelSteps[[1]]@localComm@indTrait <- matrix(10, nrow = indsLocs, ncol = 1)
 
-# now we can test if all of the sp 3 individuals are replaced by sp 1
-# which is the sp that matches the optim
-mr <- runRole(m)
+    mr <- runRole(m)
 
-# the test. total number of 1's can be `indsLocs - 1` because the last event
-# might be a dispersal of sp 2 from the metacomm and as the last event, it 
-# won't have time to die
-sum(mr@modelSteps[[3]]@localComm@indSpecies == 1) >= indsLocs - 1
+    # sp 1 should almost completely take over; allow 1 straggler (last event
+    # might be a dispersal of sp 2 which won't have time to die)
+    expect_gte(sum(mr@modelSteps[[3]]@localComm@indSpecies == 1), indsLocs - 1)
+})
 
-# now we can re-run the model but make sp 2 have same trait as sp 1 and we 
-# should see them both have about equal abundance
 
-# make spp 1 and 2 match
-m@modelSteps[[1]]@metaComm@spTrait <- matrix(c(0, 0, 10), ncol = 1)
+test_that("filtering: two spp with same trait have ~equal abundance", {
+    n <- 10000
+    indsLocs <- 100
+    p <- roleParams(niter = n,
+                    niterTimestep = n / 2,
+                    species_meta = 3,
+                    individuals_meta = 100,
+                    individuals_local = indsLocs,
+                    dispersal_prob = 1,
+                    speciation_local = 0,
+                    comp_sigma = 0.5,
+                    env_sigma = 0.5,
+                    neut_delta = 0,
+                    env_comp_delta = 1)
 
-# re-run
-mr <- runRole(m)
+    m <- roleModel(p)
+    m@modelSteps[[1]]@metaComm@spAbund <- c(0.5, 0.5, 0)
 
-# test uses binomial assumption for dist of 1's and 2's
-sum(mr@modelSteps[[3]]@localComm@indSpecies == 1) <= 
-    qbinom(1 - .Machine$double.eps^0.5, 100, 0.5) &
-    sum(mr@modelSteps[[3]]@localComm@indSpecies == 1) >= 
-    qbinom(1 - .Machine$double.eps^0.5, 100, 0.5, lower.tail = FALSE)
+    # spp 1 and 2 both match optim; should have similar abundance
+    m@modelSteps[[1]]@metaComm@spTrait <- matrix(c(0, 0, 10), ncol = 1)
+    m@modelSteps[[1]]@localComm@indSpecies <- rep(3, indsLocs)
+    m@modelSteps[[1]]@localComm@indTrait <- matrix(10, nrow = indsLocs, ncol = 1)
+
+    mr <- runRole(m)
+    nsp1 <- sum(mr@modelSteps[[3]]@localComm@indSpecies == 1)
+
+    # test uses binomial assumption for distribution of sp1 count
+    expect_lte(nsp1, qbinom(1 - .Machine$double.eps^0.5, 100, 0.5))
+    expect_gte(nsp1, qbinom(1 - .Machine$double.eps^0.5, 100, 0.5, lower.tail = FALSE))
+})
 
 
 # test that traits don't matter in neutrality ----
-n <- 10000
-indsLocs <- 100
-p <- roleParams(niter = n, 
-                niterTimestep = n / 2,
-                species_meta = 2, 
-                individuals_meta = 100, 
-                individuals_local = indsLocs, 
-                dispersal_prob = 1,
-                speciation_local = 0,
-                comp_sigma = 0.5,
-                env_sigma = 0.5,
-                neut_delta = 1, # ALL NEUTRAL DESPITE OTHER PARAMS 
-                env_comp_delta = 1)
 
-m <- roleModel(p)
+test_that("neutral model: spp 1 and 2 have ~equal abundance despite trait differences", {
+    n <- 10000
+    indsLocs <- 100
+    p <- roleParams(niter = n,
+                    niterTimestep = n / 2,
+                    species_meta = 3,
+                    individuals_meta = 100,
+                    individuals_local = indsLocs,
+                    dispersal_prob = 0.1,
+                    speciation_local = 0,
+                    comp_sigma = 0.5,
+                    env_sigma = 0.5,
+                    neut_delta = 1, # all neutral
+                    env_comp_delta = 1)
 
-# update the model to specific config for testing:
-# in metacomm, spp 1 and 2 have equal abundance, sp 3 is nill
-m@modelSteps[[1]]@metaComm@spAbund <- c(0.5, 0.5, 0)
+    m <- roleModel(p)
+    m@modelSteps[[1]]@metaComm@spAbund <- c(0.5, 0.5, 0)
+    m@modelSteps[[1]]@metaComm@spTrait <- matrix(c(0, 10, 10), ncol = 1)
+    m@modelSteps[[1]]@localComm@indSpecies <- rep(3, indsLocs)
+    m@modelSteps[[1]]@localComm@indTrait <- matrix(10, nrow = indsLocs, ncol = 1)
 
-# in metacomm, spp 1 matches optim perfectly, spp 2 and 3 are far from optim
-m@modelSteps[[1]]@metaComm@spTrait <- matrix(c(0, 10, 10), ncol = 1)
+    mr <- runRole(m)
+    nsp1 <- sum(mr@modelSteps[[3]]@localComm@indSpecies == 1)
+    nsp2 <- sum(mr@modelSteps[[3]]@localComm@indSpecies == 2)
 
-# init of local comm is all sp 3
-m@modelSteps[[1]]@localComm@indSpecies <- rep(3, indsLocs)
-m@modelSteps[[1]]@localComm@indTrait <- matrix(10, nrow = indsLocs, ncol = 1)
-
-# now we can test if sp 1 and sp 2 have approx same abund despite diff traits
-mr <- runRole(m)
-
-# the test. total number of 1's can be `indsLocs - 1` because the last event
-# might be a dispersal of sp 2 from the metacomm and as the last event, it 
-# won't have time to die
-nsp1 <- sum(mr@modelSteps[[3]]@localComm@indSpecies == 1)
-nsp2 <- sum(mr@modelSteps[[3]]@localComm@indSpecies == 2)
-
-# test again uses binomial assumption and the fact that if:
-# X ~ binom(n, 0.5) and Y ~ binom(n, 0.5), 
-# then n - Y ~ binom(n, 0.5) and X - Y + n ~ binom(2 * n, 0.5)
-nsp1 - nsp2 + indsLocs <= 
-    qbinom(1 - .Machine$double.eps^0.5, indsLocs * 2, 0.5) &
-    nsp1 - nsp2 + indsLocs >= 
-    qbinom(1 - .Machine$double.eps^0.5, indsLocs * 2, 0.5, lower.tail = FALSE)
-
+    # if X ~ binom(n, 0.5) and Y ~ binom(n, 0.5),
+    # then X - Y + n ~ binom(2*n, 0.5)
+    diff_stat <- nsp1 - nsp2 + indsLocs
+    expect_gte(diff_stat, qbinom(1 - .Machine$double.eps^0.5, indsLocs * 2, 0.5, lower.tail = FALSE))
+    expect_lte(diff_stat, qbinom(1 - .Machine$double.eps^0.5, indsLocs * 2, 0.5))
+})
 
 
 # test speciation happens only when it should ----
 
-n <- 10000
-indsLocs <- 100
-p <- roleParams(niter = n, 
-                niterTimestep = n / 2,
-                species_meta = 2, 
-                individuals_meta = 100, 
-                individuals_local = indsLocs, 
-                dispersal_prob = 0.25,
-                speciation_local = 0.001, # high ass speciation prob for testing
-                comp_sigma = 0.5,
-                env_sigma = 0.5,
-                neut_delta = 0.5, 
-                env_comp_delta = 1)
+test_that("speciation increases species richness", {
+    n <- 10000
+    p <- roleParams(niter = n,
+                    niterTimestep = n / 2,
+                    species_meta = 2,
+                    individuals_meta = 100,
+                    individuals_local = 100,
+                    dispersal_prob = 0.25,
+                    speciation_local = 0.001, # high speciation prob for testing
+                    comp_sigma = 0.5,
+                    env_sigma = 0.5,
+                    neut_delta = 0.5,
+                    env_comp_delta = 1)
 
-m <- roleModel(p)
-mr <- runRole(m)
+    m <- roleModel(p)
+    init_nspp <- m@modelSteps[[1]]@phylo@n
+    mr <- runRole(m)
+    final_nspp <- mr@modelSteps[[length(mr@modelSteps)]]@phylo@n
 
+    # with high speciation rate, final nspp should exceed initial nspp
+    expect_gt(final_nspp, init_nspp)
+})
 
 
 # test speciation updates spp IDs correctly ----
 
+test_that("speciation assigns new species IDs greater than initial species count", {
+    n <- 10000
+    p <- roleParams(niter = n,
+                    niterTimestep = n / 2,
+                    species_meta = 2,
+                    individuals_meta = 100,
+                    individuals_local = 100,
+                    dispersal_prob = 0.25,
+                    speciation_local = 0.001,
+                    comp_sigma = 0.5,
+                    env_sigma = 0.5,
+                    neut_delta = 0.5,
+                    env_comp_delta = 1)
+
+    m <- roleModel(p)
+    init_nspp <- m@modelSteps[[1]]@phylo@n
+    mr <- runRole(m)
+    final_step <- mr@modelSteps[[length(mr@modelSteps)]]
+    final_nspp <- final_step@phylo@n
+
+    if (final_nspp <= init_nspp) {
+        skip("no speciation occurred; try increasing speciation_local or niter")
+    }
+
+    # new species IDs (> init_nspp) should be present in the local community
+    all_sp_ids <- unique(final_step@localComm@indSpecies)
+    expect_true(any(all_sp_ids > init_nspp))
+})
+
 
 # test trait update with immigration ----
 
-n <- 1
-indsLocs <- 2
-p <- roleParams(niter = n, 
-                niterTimestep = 1,
-                species_meta = 2, 
-                individuals_meta = 100, 
-                individuals_local = indsLocs, 
-                dispersal_prob = 1,
-                speciation_local = 0,
-                comp_sigma = 0.5,
-                env_sigma = 0.5,
-                neut_delta = 1, # all neutral
-                env_comp_delta = 1)
+test_that("immigrating individual inherits parent species trait", {
+    n <- 1
+    indsLocs <- 2
+    p <- roleParams(niter = n,
+                    niterTimestep = 1,
+                    species_meta = 2,
+                    individuals_meta = 100,
+                    individuals_local = indsLocs,
+                    dispersal_prob = 1,
+                    speciation_local = 0,
+                    comp_sigma = 0.5,
+                    env_sigma = 0.5,
+                    neut_delta = 1, # all neutral
+                    env_comp_delta = 1)
 
-m <- roleModel(p)
+    m <- roleModel(p)
 
-# update the model to specific config for testing:
-# in metacomm, only sp 1 is present
-m@modelSteps[[1]]@metaComm@spAbund <- c(1, 0)
+    # only sp 1 in metacomm
+    m@modelSteps[[1]]@metaComm@spAbund <- c(1, 0)
 
-# in metacomm, spp have very different traits
-metaTrt <- matrix(c(-100, 100), ncol = 1)
-m@modelSteps[[1]]@metaComm@spTrait <- metaTrt
+    # spp have very different traits
+    metaTrt <- matrix(c(-100, 100), ncol = 1)
+    m@modelSteps[[1]]@metaComm@spTrait <- metaTrt
 
-# init of local comm is all sp 2
-m@modelSteps[[1]]@localComm@indSpecies <- c(2, 2)
-m@modelSteps[[1]]@localComm@indTrait <- matrix(c(100, 100), ncol = 1)
+    # init: all sp 2
+    m@modelSteps[[1]]@localComm@indSpecies <- c(2, 2)
+    m@modelSteps[[1]]@localComm@indTrait <- matrix(c(100, 100), ncol = 1)
 
-# now we can test if trait of new individual (which *has* to be of sp 1) is 
-# closer to trait of sp 1, as it should be
-mr <- runRole(m)
+    mr <- runRole(m)
 
-IDs <- mr@modelSteps[[2]]@localComm@indSpecies
-trts <- mr@modelSteps[[2]]@localComm@indTrait
+    IDs <- mr@modelSteps[[2]]@localComm@indSpecies
+    trts <- mr@modelSteps[[2]]@localComm@indTrait
 
-# the test:
-abs(trts[IDs == 1] - metaTrt[1, 1]) < abs(trts[IDs == 1] - metaTrt[2, 1])
-
-
+    # the new sp 1 individual's trait should be closer to sp 1 trait than sp 2 trait
+    expect_true(
+        abs(trts[IDs == 1] - metaTrt[1, 1]) < abs(trts[IDs == 1] - metaTrt[2, 1])
+    )
+})
 
 
 # test trait update with birth ----
 
-n <- 1
-indsLocs <- 2
-p <- roleParams(niter = n, 
-                niterTimestep = 1,
-                species_meta = 2, 
-                individuals_meta = 100, 
-                individuals_local = indsLocs, 
-                dispersal_prob = 0,
-                speciation_local = 0,
-                comp_sigma = 0.5,
-                env_sigma = 0.5,
-                neut_delta = 1, # all neutral
-                env_comp_delta = 1)
+test_that("offspring trait is closer to parent species trait than to other species", {
+    n <- 1
+    indsLocs <- 2
+    p <- roleParams(niter = n,
+                    niterTimestep = 1,
+                    species_meta = 2,
+                    individuals_meta = 100,
+                    individuals_local = indsLocs,
+                    dispersal_prob = 0,
+                    speciation_local = 0,
+                    comp_sigma = 0.5,
+                    env_sigma = 0.5,
+                    neut_delta = 1, # all neutral
+                    env_comp_delta = 1)
 
-m <- roleModel(p)
+    m <- roleModel(p)
 
-# update the model to specific config for testing:
+    # spp have very different traits
+    metaTrt <- matrix(c(-100, 100), ncol = 1)
 
-# spp have very different traits
-metaTrt <- matrix(c(-100, 100), ncol = 1)
+    # init: one of each spp
+    m@modelSteps[[1]]@localComm@indSpecies <- c(1, 2)
+    m@modelSteps[[1]]@localComm@indTrait <- matrix(c(-100, 100), ncol = 1)
 
-# init of local comm is one of each spp
-m@modelSteps[[1]]@localComm@indSpecies <- c(1, 2)
-m@modelSteps[[1]]@localComm@indTrait <- matrix(c(-100, 100), ncol = 1)
+    mr <- runRole(m)
 
-# now we can test if trait of new individual is closer to its parent 
-mr <- runRole(m)
+    trtsInit <- mr@modelSteps[[1]]@localComm@indTrait
+    trtsFinal <- mr@modelSteps[[2]]@localComm@indTrait
 
-trtsInit <- mr@modelSteps[[1]]@localComm@indTrait
-trtsFinal <- mr@modelSteps[[2]]@localComm@indTrait
+    newID <- which(trtsInit[, 1] != trtsFinal[, 1])
+    parentSp <- mr@modelSteps[[2]]@localComm@indSpecies[newID]
 
-newID <- which(trtsInit[, 1] != trtsFinal[, 1])
-parentSp <- mr@modelSteps[[2]]@localComm@indSpecies[newID]
-oldSp <- mr@modelSteps[[1]]@localComm@indSpecies[newID]
-
-
-# the test:
-abs(metaTrt[parentSp, 1] - trtsFinal[newID, 1]) < # diff of parent and offspring
-    abs(diff(metaTrt[, 1])) # diff of two spp
-
+    # offspring trait should be closer to its parent species trait than to the other species
+    expect_lt(
+        abs(metaTrt[parentSp, 1] - trtsFinal[newID, 1]),
+        abs(diff(metaTrt[, 1]))
+    )
+})
